a15b68a6dfe8d780431cf71c2ab38d0845965567
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-15 14:00:48 -0500

    Finished first 3 lessons: Basic introduction to laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalle del usuario: {$id}";
+})->where('id', '\d+');
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if($nickname) {
+        return "Bienvendido {$name}, tu apodo es {$nickname}";
+    }
+
+    return "Bienvendido {$name}, no tienes apodo";
+});
