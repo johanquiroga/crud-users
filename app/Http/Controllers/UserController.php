68447487d26bc00f540ab6af45c7aefb6178eab0
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-20 21:48:07 -0500

    Finished 6th and 7th lessons: Views in php and Blade templates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,33 @@ class UserController extends Controller
 {
     public function index()
     {
-        return 'Usuarios';
+        if (request()->has('empty')) {
+            $users = [];
+        } else {
+            $users = ['Joel','Ellie','Tess','Tommy','Bill'];
+        }
+
+        $title = 'Listado de usuarios';
+
+        return view('users', compact('title', 'users'));
     }
 
     public function show($id)
     {
-        return "Mostrando detalle del usuario: {$id}";
+        if (request()->has('empty')) {
+            $user = [];
+        } else {
+            $user = [
+                'id' => $id,
+                'name' => 'Johan Quiroga',
+                'nickname' => 'johanquiroga',
+                'website' => 'http://johanquiroga.me'
+            ];
+        }
+
+        $title = "Mostrando detalle del usuario: {$id}";
+
+        return view('details', compact('user', 'title'));
     }
 
     public function create()
