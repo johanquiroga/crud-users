1c62fe9b9dde65488b036dcd2087bae720e3b800
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-29 20:35:34 -0500

    Minor front-end changes

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,13 +5,28 @@
 @section('content')
     <h1>{{ $title }}</h1>
 
-    <ul>
-        @forelse ($users as $user)
-            <li>{{ $user->name }}, ({{ $user->email }}</li>
-        @empty
-            <li>No hay usuarios registrados.</li>
-        @endforelse
-    </ul>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th class="text-center">Id</th>
+                <th class="text-center">Name</th>
+                <th class="text-center">E-mail</th>
+            </tr>
+            </thead>
+            <tbody>
+                @forelse ($users as $user)
+                    <tr>
+                        <td class="text-center">{{ $user->id }}</td>
+                        <td class="text-center">{{ $user->name }}</td>
+                        <td class="text-center">{{ $user->email }}</td>
+                    </tr>
+                @empty
+                    <tr><td colspan="3">No hay usuarios registrados.</td></tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 @section('sidebar')
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,10 +3,23 @@
 @section('title', "Usuario {$user->id}")
 
 @section('content')
-    <h1>Usuario #{{ $user->id }}</h1>
-
-    <ul>
-        <li>Nombre del usuario: {{ $user->name }}</li>
-        <li>Correo electrónico: {{ $user->email }}</li>
-    </ul>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <td colspan="2" class="text-center"><strong>Usuario #{{ $user->id }}</strong></td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="text-center">Nombre del usuario:</td>
+                    <td class="text-center">{{ $user->name }}</td>
+                </tr>
+                <tr>
+                    <td class="text-center">E-mail:</td>
+                    <td class="text-center">{{ $user->email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
